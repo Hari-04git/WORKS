7f043eb0c4d855e6ada4d807e44454c3592e5ba2
Hari-04git <sudhann.1916@gmail.com>
2025-06-22 15:00:39 +0530

    Add files via upload

---
+++ Grade.java
@@ -0,0 +1,15 @@
+public class grade {
+    public static void main(String[] args) {
+        int mark=80;
+        if(mark>=75){
+            System.out.println("Grade=A"); 
+            }
+            else if(mark>=60){
+                System.out.println("Grade=B");
+            }
+                else{
+                    System.out.println("Grade=C");
+                }
+    }
+}
+       
