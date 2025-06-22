bf724a1784ad82f5ed5ebf3cd0a986a6a141feb5
Hari-04git <sudhann.1916@gmail.com>
2025-06-22 15:18:30 +0530

    Add files via upload

---
+++ reverse array.java
@@ -0,0 +1,12 @@
+public class ReverseArray {
+    public static void main(String[] args) {
+        int[] arr = {5,4,3,2,1};
+        for(int i=0;i<arr.length;i++){
+         System.out.print(arr[i]);
+        }
+        System.out.println();
+        for(int i=4;i>=0;i--){
+         System.out.println(arr[i]);
+}
+}
+}
