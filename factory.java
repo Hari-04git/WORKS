3478652bf75fc986c6bf84e7b4de3683b24757fd
Hari-04git <sudhann.1916@gmail.com>
2025-07-14 19:58:37 +0530

    factory.interface

---
+++ factory.java
@@ -0,0 +1,40 @@
+ackage assign;
+
+interface factory{
+    void salary();
+}
+
+
+class Employer implements factory{
+    public void salary() {
+        System.out.println("Salary = 80,000");
+    }
+}
+
+class Manager implements factory{
+    public void salary(){
+        System.out.println("Salary = 70,000");
+    }
+}
+
+class Employee implements factory{
+    public void salary() {
+        System.out.println("Salary = 50,000");
+    }
+}
+
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Employer emp = new Employer();
+        emp.salary();
+
+        Manager mng=new Manager();
+        mng.salary();
+
+       
+        Employee empl = new Employee();
+        empl.salary();
+    }
+}
